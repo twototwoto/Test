93c122e6156e447ee13f9597fdebe3ef8765c61e
WYW <15076509228@163.com>
2017-01-12 22:27:45 +0800

    第一次提交代码到git

---
+++ TestGit/TestGit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestGit
+//
+//  Created by Live on 2017/1/12.
+//  Copyright © 2017年 ITCoderW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"第一次提交代码到git");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
