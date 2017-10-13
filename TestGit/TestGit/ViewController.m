132b16f0847a40fd5cfefc95b2dab15ac7d3e818
testName <2637405168@qq.com>
2017-10-13 15:57:23 +0800

    newBranch 再次提交

--- TestGit/TestGit/ViewController.m
+++ TestGit/TestGit/ViewController.m
@@ -19,6 +19,8 @@
     NSLog(@"第一次提交代码到git");
     NSLog(@"第二次提交代码到git");
     NSLog(@"创建并且切换到新的分支newBranch 提交信息");
+    NSLog(@"切换分支：git checkout master -->合并分支： git merge newBranch");
+    NSLog(@"再次切换到newBranch:git checkout newBranch");
     
 }
 
